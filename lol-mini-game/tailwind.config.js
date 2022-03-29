8ce6227301438d921e824a3703e336302da7b872
오해성 <gotjd2720@gmail.com>
2022-03-30 00:15:45 +0900

    [SETUP] next js + tailwind + typescript setup and add images, fonts ext...

---
+++ lol-mini-game/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    colors: {
+      "black-100": "#222222",
+      "black-200": "#1b1f23",
+      "brown-100": "#e1dbca",
+      "brown-200": "#b59758",
+      "brown-300": "#947342",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
