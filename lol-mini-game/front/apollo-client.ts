377f80f0b6c6c55ec8dc14c68d063a0328814be4
오해성 <gotjd2720@gmail.com>
2022-09-01 15:27:36 +0900

    change apllo client host

--- lol-mini-game/front/apollo-client.ts
+++ lol-mini-game/front/apollo-client.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4002/graphql',
+  uri: 'http://minigame.our.gg:4002/graphql',
   cache: new InMemoryCache(),
 });
 
